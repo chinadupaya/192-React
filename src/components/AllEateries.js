715f10cc6d1497da1db36d82f015f0af7a7de49e
dylanTRB6 <dylanbayona@yahoo.com>
2020-02-11 20:37:22 +0800

    code reviewed (2/11/2020)
    added extra docu, changed tabs to spaces

--- src/components/AllEateries.js
+++ src/components/AllEateries.js
@@ -13,6 +13,7 @@ of the Philippines, Diliman for the AY 2019-
 1/20/20: Annysia Dupaya - Created component
 1/25/20: Annysia Dupaya - Integrated with API
 2/6/20: Annysia Dupaya - DOes not show eatery if flagged
+2/11/20: Dylan Bayona - Reviewed code
 
 ---ABOUT---
 File creation date: 1/20/20
@@ -27,10 +28,10 @@ import PhoneAndroidIcon from '@material-ui/icons/PhoneAndroid';
 import PlaceIcon from '@material-ui/icons/Place';
 import Box from '@material-ui/core/Box';
 import {
-    Link,
-    Switch,
-    Route
-  } from "react-router-dom";
+     Link,
+     Switch,
+     Route
+} from "react-router-dom";
 import Eatery from './Eatery';
 import StarIcon from '@material-ui/icons/Star';
 import '../stylesheets/AllEateries.css';
@@ -46,43 +47,60 @@ List of required files/database tables: N/A
 Return value: Rendered page
 */
 const AllEateries = (props) =>{
-    const flagCheck2 = ()=>{
-        props.flagCheck();
-    }
-    var eateries = props.eateries.map((eatery)=>{
-        if(eatery.flag){
-            return null;
-        }
-        return(
-            <Box key={eatery.id} className = 'eateryBox' border={1} borderColor={grey[300]}>
-                <div className='subBox'>
-                    <Typography component={Link} 
-                        to={"eatery/"+eatery.id} 
-                        variant="h5" 
-                        style={{textDecoration:'none'}} 
-                        color="inherit">
-                        <strong>{eatery.name}</strong>
-                    </Typography>
-                    <p><PlaceIcon/>{eatery.address}</p>
-                </div>
-                <div className='subBox'>
-                    <Typography variant="h5"><StarIcon style={{color:orange[500]}} fontSize='large'/>{eatery.rating}</Typography>
-                    <Typography variant="subtitle1" color="textSecondary">
-                        <PhoneAndroidIcon/>{eatery.contact}
-                    </Typography>
-                </div>
+     /* ---METHOD---
+     Name: flagCheck2
+     Routine creation date: 2/6/20
+     Purpose of the routine: checks flag
+     List of calling Arguments: N/A
+     List of required files/database tables: N/A
+     Return value: N/A
+     */
+     const flagCheck2 = ()=>{
+          props.flagCheck();
+     }
+	
+     /* ---METHOD---
+     Name: eateries
+     Routine creation date: 2/6/20
+     Purpose of the routine: returns eateries, if eatery is flagged, it is not returned
+     List of calling Arguments: eatery
+     List of required files/database tables: Eatery
+     Return value: null/eatery
+     */	
+     var eateries = props.eateries.map((eatery)=>{
+          if(eatery.flag){
+               return null;
+          }
+          return(
+               <Box key={eatery.id} className = 'eateryBox' border={1} borderColor={grey[300]}>
+                    <div className='subBox'>
+                         <Typography component={Link} 
+                             to={"eatery/"+eatery.id} 
+                             variant="h5" 
+                             style={{textDecoration:'none'}} 
+                             color="inherit">
+                             <strong>{eatery.name}</strong>
+                         </Typography>
+                         <p><PlaceIcon/>{eatery.address}</p>
+                     </div>
+                     <div className='subBox'>
+                         <Typography variant="h5"><StarIcon style={{color:orange[500]}} fontSize='large'/>{eatery.rating}</Typography>
+                         <Typography variant="subtitle1" color="textSecondary">
+                              <PhoneAndroidIcon/>{eatery.contact}
+                         </Typography>
+                     </div>
                 
-            </Box>
-        )
-    })
-    return(
-        <div className = "ui cards">
-            <Switch>
-                <Route exact path="/eatery" render={()=>(<div className = 'eateriesContainer'>{eateries}</div>)}/>
-                <Route path="/eatery/:id" render ={(props)=><Eatery {...props} flagCheck2={flagCheck2}/>}/>
-            </Switch>
-        </div>
-    )
+               </Box>
+         )
+     })
+     return(
+          <div className = "ui cards">
+               <Switch>
+                    <Route exact path="/eatery" render={()=>(<div className = 'eateriesContainer'>{eateries}</div>)}/>
+                    <Route path="/eatery/:id" render ={(props)=><Eatery {...props} flagCheck2={flagCheck2}/>}/>
+               </Switch>
+          </div>
+     )
 }
 
 export default AllEateries;
