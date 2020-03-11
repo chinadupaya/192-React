ce10ea27cd440c5267dca62d3441da07c9d6abdb
Annyia Dupaya <dupayag@gmail.com>
2020-03-11 14:22:41 +0800

    edit eatery

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -28,6 +28,36 @@ import Box from '@material-ui/core/Box';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import MuiPhoneNumber from 'material-ui-phone-number';
+import TextField from '@material-ui/core/TextField';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+
+
+/* ---METHOD---
+Name: useStyles
+Routine creation date: 3/3/20
+Purpose of the routine: Used to apply styles to this component
+List of calling arguments: theme
+List of required files/database tables: N/A
+Return value: N/A
+*/
+const useStyles = makeStyles(theme => ({
+    modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    paper: {
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
 /* ---METHOD---
 Name: TabPanel
 Routine creation date: 2/26/20
@@ -82,13 +112,18 @@ Return value: Returns Admin component
 */
 const Admin=(props)=>{
 
+    
+
     let count=0;
     let reviewCount=0;
     const [value, setValue] = React.useState(0);
+    const [phone, setPhone] = useState();
+    let formFields = {}
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
+    const classes = useStyles();
     /* ---METHOD---
     Name: eateries
     Routine creation date: 2/26/20
@@ -97,7 +132,26 @@ const Admin=(props)=>{
     List of required files/database tables: N/A
     Return value: rendered eateries
     */
+
+   const [open, setOpen] = React.useState(false);
+   const handleOpen = () => {
+       setOpen(true);
+   };
+
+   /* ---METHOD---
+   Name: handleClose
+   Routine creation date: 3/3/20
+   Purpose of the routine: Changes variable setOpen to false
+   List of calling arguments: N/A
+   List of required files/database tables: N/A
+   Return value: N/A
+   */  
+   const handleClose = () => {
+   setOpen(false);
+   };
     var eateries = props.eateries.map((eatery)=>{
+        
+        
         /* ---METHOD---
         Name: handleEateryUnflag
         Routine creation date: 2/26/20
@@ -118,14 +172,132 @@ const Admin=(props)=>{
         if(!eatery.flag){
             return null;
         }
+         /* ---METHOD---
+        Name: handleEditEaterySubmit
+        Routine creation date: 3/11/20
+        Purpose of the routine: Provides functionality to Admin, unflags a flagged eatery
+        List of calling arguments: props
+        List of required files/database tables: N/A
+        Return value: null
+        */
+       function handleEditEaterySubmit(name,address,contact){
+            let body = {name:name, address:address, contact:contact};
+           fetch('http://localhost:5000/eatery/'+eatery.id+'/update',{
+               method:'PUT',
+               headers: {
+                'Content-type': 'application/json'
+                },
+                body: JSON.stringify(body)
+           }).then((response)=>{return response.json()})
+           .then(()=>{handleEateryUnflag()})
+       }
         count+=1;
-        return(<Box key={eatery.id} className="eateryBox"  border={1}>
+        return(
+        
+        <Box key={eatery.id} className="eateryBox"  border={1}>
+            <Modal
+               aria-labelledby="transition-modal-title"
+               aria-describedby="transition-modal-description"
+               className={classes.modal}
+               open={open}
+               onClose={handleClose}
+               closeAfterTransition
+               BackdropComponent={Backdrop}
+               BackdropProps={{
+                    timeout: 500,
+               }}
+            >
+
+        <Fade in={open}>
+          
+          <div className={classes.paper}>
+               <form noValidate autoComplete="off" 
+                onSubmit={(e)=> {handleEditEaterySubmit(formFields.name.value,
+                formFields.address.value, 
+                phone+"");
+			    e.target.reset();}}>
+               <div className="field">
+                     <TextField id="outlined-basic" inputRef={input => formFields.name = input} label={eatery.name} variant="outlined" />
+                
+                     {/* <input ref={input => formFields.name = input} type="text" id="name" placeholder="Name" /> */}
+               </div>
+               <div className="field">
+                    <InputLabel>Address</InputLabel>
+                    <Select native className="form-control"id="select1" inputRef={input => formFields.address = input}>
+                         <option>College of Architecture</option>
+                         <optgroup label="Archaeological Studies Program">
+                              <option>Albert Hall</option>
+                         </optgroup>
+                         <option>College of Arts and Letters</option>
+                         <optgroup label="College of Home Economics">
+                              <option>Alonso Hall</option>
+                              <option>Gusali 2</option>
+                         </optgroup>
+                         <option>College of Human Kinetics</option>
+                         <option>College of Mass Communication</option>
+                         <option>College of Music</option>
+                         <optgroup label="College of Science">
+                              <option>Institute of Biology</option>
+                              <option>Zoology Building/CASAA</option>
+                              <option>Institute of Chemistry</option>
+                              <option>National Institute of Geological Sciences</option>
+                              <option>CS Library</option>
+                              <option>Institute of Mathematics</option>
+                              <option>National Institute of Molecular Biology and Biotechnology</option>
+                              <option>National Institute of Physics</option>
+                         </optgroup>
+                         <optgroup label="College of Social Sciences and Philosophy">
+                              <option>Palma Hall</option>
+                              <option>Lagmay Hall</option>
+                         </optgroup>
+                              <option>School of Economics</option>
+                         <optgroup label="College of Education">
+                              <option>Benitez Hall</option>
+                              <option>Vidal Tan Hall</option>
+                              <option>UP Integrated School</option>
+                         </optgroup>
+                         <optgroup label="College of Engineering">
+                              <option>Melchor Hall</option>
+                              <option>UP Alumni Engineers Centennial Hall</option>
+                              <option>Institute of Civil Engineering</option>
+                              <option>Industrial Engineering – Mechanical Engineering Building</option>
+                              <option>Electrical and Electronics Engineering Institute</option>
+                         </optgroup>
+                              <option>College of Law</option>
+                              <option>School of Statistics</option>
+                              <option>School of Urban and Regional Planning</option>
+                              <option>Cesar E.A. Virata School of Business</option>
+                              <option>University Library</option>
+                              <option>Vargas Museum</option>
+                         <optgroup label="Others">
+                              <option>Bahay ng Alumni</option>
+                              <option>Area 2</option>
+                              <option>OTHERS</option>
+                         </optgroup>
+                     </Select>
+                 </div>
+                 <div className="field">
+                 <MuiPhoneNumber 
+                     //inputRef={input => formFields.contact = input} 
+                     value = {phone}
+                     defaultCountry={'ph'} 
+                     onlyCountries={['ph']} 
+                     onChange={(e)=>{setPhone(e);}}/>
+                 </div>
+                 <button className="ui primary button"type="submit">Edit and Unflag Eatery</button>
+               </form>
+           </div>
+           </Fade>
+            </Modal>
             <div className="eateryInfo subBox">
                 <Typography component="h5" variant="h5"><strong>{eatery.name}</strong></Typography>
                 {eatery.why_flag}
             </div>
             <div className="eateryActions subBox">
-                <Button variant="contained" onClick={handleEateryUnflag}color="primary">Unflag Eatery</Button> <Button variant="contained" color="secondary">Delete Eatery</Button>
+                <Button variant="contained" onClick={handleEateryUnflag}color="primary">Unflag Eatery</Button> 
+                <Button variant="contained" color="secondary">Delete Eatery</Button>
+                <Button variant="contained" onClick={handleOpen}>Edit Eatery</Button>
+            
             </div>
             </Box>
         )
@@ -140,6 +312,7 @@ const Admin=(props)=>{
     */
 
     var flaggedReviews = props.flaggedReviews.map((fReview) =>{
+
         /* ---METHOD---
         Name: handleReviewUnflag
         Routine creation date: 2/26/20
@@ -166,7 +339,10 @@ const Admin=(props)=>{
             return null;
         }
         reviewCount+=1;
-        return(<Box key={fReview.id} className="eateryBox"  border={1}>
+        return(
+        
+        <Box key={fReview.id} className="eateryBox"  border={1}>
+            
         <div className="eateryInfo subBox">
             <Typography component="h5" variant="h5"><strong>{fReview.review_text}</strong></Typography>
             {fReview.why_flag}
@@ -183,9 +359,9 @@ const Admin=(props)=>{
     })
 
     let eatery_length = count
-
     return (
         <div className="Admin">
+            
             <div className = "adminHeader">
                 Welcome to the RUPE admin page!
             </div>
